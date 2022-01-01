f11f6fefa00c8fd5caa316e0547eb4c8fca3d35f
ErElectical <mohittanwar786.mt@gmail.com>
2022-01-01 09:46:54 +0000

    Main Method

--- Program.cs
+++ Program.cs
@@ -10,8 +10,19 @@ namespace RegexProgram
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to regex Programs ");
+            Console.WriteLine("validate your First name ");
             Patterns pat = new Patterns();
             pat.Pattern();
+
+            Console.WriteLine("validate your last name ");
+           
+            Console.WriteLine("enter your last name ");
+        
+
+            string l_name =Console.ReadLine();
+            Lastname ls = new Lastname();
+
+            ls.checkLastName(l_name);
 
         }
     }
