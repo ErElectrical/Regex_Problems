1e1aeb25f3bdcbcf816e82061c8349310b6b9e34
ErElectrical <94623283+ErElectrical@users.noreply.github.com>
2022-01-01 12:43:04 +0000

    password rule 3

---
+++ password3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexProgram
+{
+    public  class password3
+    {
+        public static string Regex_Password = "^((?=.*[A-Z])(?=.*[0-9])(?=.{8,}))";
+        public static bool validatePassword(string password)
+        {
+            return Regex.IsMatch(password, Regex_Password);
+        }
+
+        public static void checkPassword(string password)
+        {
+            if (Regex.IsMatch(password, Regex_Password) == false)
+            {
+                Console.WriteLine("Enter valid password. It should contain minimum 8 character," +
+                    "atleast one upper case character and atleast one numeric value");
+            }
+        }
+    }
+}
