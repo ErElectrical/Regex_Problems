2beceaa4cd3662a91a30865c2b96145ddcccdea1
ErElectical <mohittanwar786.mt@gmail.com>
2022-01-01 12:21:09 +0000

    Password file

---
+++ Password1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexProgram
+{
+    public  class Password1
+    {
+        public static string Regex_Password = "^[A-Za-z]{8,}";
+            public static bool validatePassword(string password)
+        {
+            return Regex.IsMatch(password, Regex_Password);
+        }
+
+        public static void checkPassword(string password)
+        {
+            if (Regex.IsMatch(password, Regex_Password) == false)
+            {
+                Console.WriteLine("Enter valid password. It should contain minimum 8 character!");
+            }
+            else
+            {
+                Console.WriteLine("password accepted ");
+            }
+        }
+
+    }
+}
